02564f440193327d542ee2081e021ff0d0fed30c
SURYANSH8800 <suryanshpandey8800@gmail.com>
2025-11-09 22:21:54 +0530

    Time: 19 ms (25.34%), Space: 45.1 MB (11.97%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        return helper(root,(long)targetSum)+pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+    }
+
+    private int helper(TreeNode root, long sum) {
+        if (root == null) return 0;
+
+        int res = 0;
+        if (root.val == sum) res++;
+
+        res += helper(root.left, sum - root.val);
+        res += helper(root.right, sum - root.val);
+
+        return res;
+    }
+}
